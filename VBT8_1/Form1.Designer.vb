676cf0d8f60ec088f9463fea3663a95006e4fce2
孫氷 <sunbingms@hotmail.com>
2019-11-05 14:25:30 +0900

    プロジェクト ファイルを追加します。

---
+++ VBT8_1/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.URLToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpacityToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem020 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem040 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem060 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem080 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem100 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AxShockwaveFlash1 = New AxShockwaveFlashObjects.AxShockwaveFlash()
+        Me.WebBrowser = New System.Windows.Forms.WebBrowser()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.AxShockwaveFlash1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.URLToolStripMenuItem, Me.HideShowToolStripMenuItem, Me.OpacityToolStripMenuItem, Me.ToolStripSeparator1, Me.CloseToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(134, 98)
+        '
+        'URLToolStripMenuItem
+        '
+        Me.URLToolStripMenuItem.Image = CType(resources.GetObject("URLToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.URLToolStripMenuItem.Name = "URLToolStripMenuItem"
+        Me.URLToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.URLToolStripMenuItem.Text = "URL"
+        '
+        'HideShowToolStripMenuItem
+        '
+        Me.HideShowToolStripMenuItem.Image = CType(resources.GetObject("HideShowToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.HideShowToolStripMenuItem.Name = "HideShowToolStripMenuItem"
+        Me.HideShowToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.HideShowToolStripMenuItem.Text = "Hide/Show"
+        '
+        'OpacityToolStripMenuItem
+        '
+        Me.OpacityToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem020, Me.ToolStripMenuItem040, Me.ToolStripMenuItem060, Me.ToolStripMenuItem080, Me.ToolStripMenuItem100})
+        Me.OpacityToolStripMenuItem.Image = CType(resources.GetObject("OpacityToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.OpacityToolStripMenuItem.Name = "OpacityToolStripMenuItem"
+        Me.OpacityToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.OpacityToolStripMenuItem.Text = "Opacity"
+        '
+        'ToolStripMenuItem020
+        '
+        Me.ToolStripMenuItem020.Name = "ToolStripMenuItem020"
+        Me.ToolStripMenuItem020.Size = New System.Drawing.Size(102, 22)
+        Me.ToolStripMenuItem020.Text = "20%"
+        '
+        'ToolStripMenuItem040
+        '
+        Me.ToolStripMenuItem040.Name = "ToolStripMenuItem040"
+        Me.ToolStripMenuItem040.Size = New System.Drawing.Size(102, 22)
+        Me.ToolStripMenuItem040.Text = "40%"
+        '
+        'ToolStripMenuItem060
+        '
+        Me.ToolStripMenuItem060.Name = "ToolStripMenuItem060"
+        Me.ToolStripMenuItem060.Size = New System.Drawing.Size(102, 22)
+        Me.ToolStripMenuItem060.Text = "60%"
+        '
+        'ToolStripMenuItem080
+        '
+        Me.ToolStripMenuItem080.Name = "ToolStripMenuItem080"
+        Me.ToolStripMenuItem080.Size = New System.Drawing.Size(102, 22)
+        Me.ToolStripMenuItem080.Text = "80%"
+        '
+        'ToolStripMenuItem100
+        '
+        Me.ToolStripMenuItem100.Name = "ToolStripMenuItem100"
+        Me.ToolStripMenuItem100.Size = New System.Drawing.Size(102, 22)
+        Me.ToolStripMenuItem100.Text = "100%"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(130, 6)
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Image = CType(resources.GetObject("CloseToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'AxShockwaveFlash1
+        '
+        Me.AxShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.AxShockwaveFlash1.Enabled = True
+        Me.AxShockwaveFlash1.Location = New System.Drawing.Point(0, 0)
+        Me.AxShockwaveFlash1.Name = "AxShockwaveFlash1"
+        Me.AxShockwaveFlash1.OcxState = CType(resources.GetObject("AxShockwaveFlash1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxShockwaveFlash1.Size = New System.Drawing.Size(120, 90)
+        Me.AxShockwaveFlash1.TabIndex = 0
+        '
+        'WebBrowser
+        '
+        Me.WebBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser.Name = "WebBrowser"
+        Me.WebBrowser.Size = New System.Drawing.Size(120, 90)
+        Me.WebBrowser.TabIndex = 1
+        Me.WebBrowser.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(120, 90)
+        Me.Controls.Add(Me.WebBrowser)
+        Me.Controls.Add(Me.AxShockwaveFlash1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.AxShockwaveFlash1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents AxShockwaveFlash1 As AxShockwaveFlashObjects.AxShockwaveFlash
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents URLToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HideShowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpacityToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem020 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem040 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem060 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem080 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem100 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents WebBrowser As System.Windows.Forms.WebBrowser
+
+End Class
---
+++ VBT8_1/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+#Region "Var"
+
+    Public Const MOD_ALT As Integer = &H1 'Alt key
+    Public Const WM_HOTKEY As Integer = &H312
+
+    <DllImport("User32.dll")> _
+    Public Shared Function RegisterHotKey(ByVal hwnd As IntPtr, _
+                        ByVal id As Integer, ByVal fsModifiers As Integer, _
+                        ByVal vk As Integer) As Integer
+    End Function
+
+    <DllImport("User32.dll")> _
+    Public Shared Function UnregisterHotKey(ByVal hwnd As IntPtr, _
+                        ByVal id As Integer) As Integer
+    End Function
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_HOTKEY Then
+            Dim id As IntPtr = m.WParam
+            Select Case (id.ToString)
+                Case "100"
+                    AxShockwaveFlash1.Stop()
+                    Me.TopMost = False
+                    Me.Visible = False
+                Case "200"
+                    Call HideAndShow()
+                Case "300"
+                    Call SetURL()
+                Case "400"
+                    Call SwitchMode()
+            End Select
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+#End Region
+
+#Region "Form"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AxShockwaveFlash1.Visible = True
+        WebBrowser.Visible = False
+        RegisterHotKey(Me.Handle, 100, MOD_ALT, Keys.Z)
+        RegisterHotKey(Me.Handle, 200, MOD_ALT, Keys.X)
+        RegisterHotKey(Me.Handle, 300, MOD_ALT, Keys.A)
+        RegisterHotKey(Me.Handle, 400, MOD_ALT, Keys.Q)
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        UnregisterHotKey(Me.Handle, 100)
+        UnregisterHotKey(Me.Handle, 200)
+        UnregisterHotKey(Me.Handle, 300)
+        UnregisterHotKey(Me.Handle, 400)
+    End Sub
+
+#End Region
+
+#Region "TrayMenu"
+
+    Private Sub URLToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles URLToolStripMenuItem.Click
+        Call SetURL()
+    End Sub
+
+    Private Sub HideShowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HideShowToolStripMenuItem.Click
+        Call HideAndShow()
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(sender As Object, e As EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Visible = True
+        Me.TopMost = True
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem020.Click
+        Me.Opacity = 0.2
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem040.Click
+        Me.Opacity = 0.4
+    End Sub
+
+    Private Sub ToolStripMenuItem5_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem060.Click
+        Me.Opacity = 0.6
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem080.Click
+        Me.Opacity = 0.8
+    End Sub
+
+    Private Sub ToolStripMenuItem6_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem100.Click
+        Me.Opacity = 1.0
+    End Sub
+
+#End Region
+
+#Region "Process"
+
+    Private Sub SetURL()
+        Dim url As String = String.Empty
+        If AxShockwaveFlash1.Visible = True Then
+            url = InputBox("url", "Input", "").Replace("/watch?v=", "/v/")
+        Else
+            url = InputBox("url", "Input", "")
+        End If
+        If url.Equals("") Then
+            Exit Sub
+        End If
+        If AxShockwaveFlash1.Visible = True Then
+            If Not url.StartsWith("https://") Then
+                url = "https://" & url
+            End If
+            AxShockwaveFlash1.Movie = url
+        Else
+            WebBrowser.Navigate(url)
+        End If
+
+    End Sub
+
+    Private Sub HideAndShow()
+        If Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None Then
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.SizableToolWindow
+        Else
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        End If
+    End Sub
+
+    Private Sub SwitchMode()
+        If AxShockwaveFlash1.Visible Then
+            AxShockwaveFlash1.Visible = False
+            WebBrowser.Visible = True
+        Else
+            AxShockwaveFlash1.Visible = True
+            WebBrowser.Visible = False
+        End If
+    End Sub
+
+#End Region
+
+    Private Sub WebBrowser_NewWindow(sender As Object, e As CancelEventArgs) Handles WebBrowser.NewWindow
+        e.Cancel = True
+        WebBrowser.Navigate(WebBrowser.StatusText)
+    End Sub
+End Class
